164ef140fca82d2e1ec80f66bdb97f7500070b42
ahmad irfan <ahmadrfn9@gmail.com>
2023-04-18 01:03:51 +0800

    dbc

--- MosqueManagement/Controllers/UserController.cs
+++ MosqueManagement/Controllers/UserController.cs
@@ -5,12 +5,80 @@ using MosqueManagement.ViewModels;
 using MosqueManagement.Models;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
+using Microsoft.AspNet.Identity;
+using System.Net.Mail;
 
 namespace MosqueManagement.Controllers
 {
     public class UserController : Controller
     {
+        private static bool emailValid(string email)
+        {
+            var valid = true;
 
+            try
+            {
+                var emailAddress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+
+        // register function
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            // Assert that the response object is not null
+            Contract.Assert(response != null, "response object must not be null");
+
+            // Invariant that the response object is valid
+            Contract.Invariant(response != null, "response object must not be null");
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            // Precondition: registerViewModel must not be null
+            Contract.Requires(registerViewModel != null, "LoginViewModel is null");
+
+            // Invariant that the email address is valid
+            Contract.Invariant(emailValid(registerViewModel.EmailAddress), "Email entered is not valid");
+            bool dupemail = false;
+            if(HttpContext.Session.GetString("email") == registerViewModel.EmailAddress)
+            {
+                dupemail = true;
+            }
+            // Assert that the email not exist
+            Contract.Assert(dupemail != true, "This email address is already in use");
+            if (dupemail)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerViewModel);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                // Invariant: ModelState.IsValid should be true when registerViewModel is valid
+                Contract.Invariant(false, "ModelState.IsValid is false when registerViewModel is valid");
+                return View(registerViewModel);
+            }
+            HttpContext.Session.SetString("email", registerViewModel.EmailAddress);
+            HttpContext.Session.SetString("password", registerViewModel.Password);
+
+            // Postcondition: the action should redirect to the Index page of the Race controller
+            Contract.Ensures(this.ControllerContext.RouteData.Values["controller"].ToString() == "User", "Action does not redirect to User/Index");
+            Contract.Ensures(this.ControllerContext.RouteData.Values["action"].ToString() == "Index", "Action does not redirect to User/Index");
+
+            return RedirectToAction("Login", "User");
+        }
+
+        // login function
         [HttpGet]
         public IActionResult Login()
         {
@@ -36,19 +104,20 @@ namespace MosqueManagement.Controllers
                 Contract.Invariant(false, "ModelState.IsValid is false when LoginViewModel is valid");
                 return View(loginViewModel);
             }
-            string validemail = "constructor@gmail.com";
-            string validpassword = "constructor123";
-            var user = "";
+
+            bool emailCheck = false;
             bool passwordCheck = false;
+            string validemail = HttpContext.Session.GetString("email");
+            string validpassword = HttpContext.Session.GetString("password");
             if (loginViewModel?.EmailAddress == validemail)
             {
-                user = validemail;
+                emailCheck = true;
             }
             
-            if (user != "")
+            if (emailCheck)
             {
                 //User is found, check password
-                if(loginViewModel.Password == validpassword)
+                if(loginViewModel.Password == validemail)
                 {
                     passwordCheck = true;
                 }
@@ -78,13 +147,13 @@ namespace MosqueManagement.Controllers
             
         }
         
-        /*
+        // logout function
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
-            await _signInManager.SignOutAsync();
-            return RedirectToAction("Index");
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "User");
         }
-        */
+        
     }
 }
--- MosqueManagement/Program.cs
+++ MosqueManagement/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddSession();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -31,6 +33,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
