e00f2e9443c69bbeff8ebb18175a1ca6b9d3b3d9
ahmad irfan <ahmadrfn9@gmail.com>
2023-04-17 21:34:45 +0800

    login

--- MosqueManagement/Controllers/UserController.cs
+++ MosqueManagement/Controllers/UserController.cs
@@ -10,18 +10,6 @@ namespace MosqueManagement.Controllers
 {
     public class UserController : Controller
     {
-        private readonly UserManager<User> _userManager;
-        private readonly SignInManager<User> _signInManager;
-        private readonly ApplicationDbContext _context;
-
-        public UserController(UserManager<User> userManager,
-            SignInManager<User> signInManager,
-            ApplicationDbContext context)
-        {
-            _context = context;
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
 
         [HttpGet]
         public IActionResult Login()
@@ -48,24 +36,29 @@ namespace MosqueManagement.Controllers
                 Contract.Invariant(false, "ModelState.IsValid is false when LoginViewModel is valid");
                 return View(loginViewModel);
             }
-
-            var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
-
-            if (user != null)
+            string validemail = "constructor@gmail.com";
+            string validpassword = "constructor123";
+            var user = "";
+            bool passwordCheck = false;
+            if (loginViewModel?.EmailAddress == validemail)
+            {
+                user = validemail;
+            }
+            
+            if (user != "")
             {
                 //User is found, check password
-                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+                if(loginViewModel.Password == validpassword)
+                {
+                    passwordCheck = true;
+                }
                 if (passwordCheck)
                 {
                     //Password correct, sign in
-                    var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
-                    if (result.Succeeded)
-                    {
                         // Postcondition: the action should redirect to the Index page of the User controller
                         Contract.Ensures(this.ControllerContext.RouteData.Values["controller"].ToString() == "User", "Action does not redirect to User/Index");
                         Contract.Ensures(this.ControllerContext.RouteData.Values["action"].ToString() == "Index", "Action does not redirect to User/Index");
-                        return RedirectToAction("Index", "User");
-                    }
+                        return RedirectToAction("Index", "Home");
                 }
 
                 //Password is incorrect
@@ -77,18 +70,21 @@ namespace MosqueManagement.Controllers
             }
 
             //User not found
-            TempData["Error"] = "Wrong credentials. Please try again";
+            TempData["Error"] = "User not found";
 
             // Invariant: TempData["Error"] should not be null or empty
             Contract.Invariant(!string.IsNullOrEmpty(TempData["Error"] as string), "TempData[\"Error\"] is null or empty");
             return View(loginViewModel);
+            
         }
-
+        
+        /*
         [HttpGet]
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index");
         }
+        */
     }
 }
